9e4ad3012c98e1affa3330e6fac05bb736c05617
MeoWoo6 <meowoo666@gmail.com>
2021-08-14 09:26:58 +0200

    Move Leetcode

---
+++ leetcode/src/ds_linkedlist/Node.java
@@ -0,0 +1,12 @@
+package ds_linkedlist;
+
+
+public class Node 
+{
+	int val;
+	Node next;
+	Node(int x)
+	{
+		val = x;
+	}
+}
---
+++ leetcode/src/ds_linkedlist/dbNode.java
@@ -0,0 +1,12 @@
+package ds_linkedlist;
+
+public class dbNode 
+{
+	int val;
+	dbNode next;
+	dbNode prev;
+	dbNode(int x)
+	{
+		val = x;
+	}
+}
