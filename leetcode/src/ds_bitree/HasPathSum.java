146eca836f89bb7c1d616c9120cece41ca7667cd
MeoWoo6 <meowoo666@gmail.com>
2021-08-27 18:27:52 +0200

    Binary Tree Path Sum

---
+++ leetcode/src/ds_bitree/HasPathSum.java
@@ -0,0 +1,15 @@
+package ds_bitree;
+
+public class HasPathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+
+        if(root.left == null && root.right == null){
+            return root.val == targetSum;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
